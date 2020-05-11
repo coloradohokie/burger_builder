17fd37dd796c53fe87409677ce7ec29cb74f37fb
Michael Newman <coloradohokie@gmail.com>
2020-05-10 20:48:09 -0600

    add Checkout; install react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from './App.module.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout'
 
 class App extends Component {
   render() {
