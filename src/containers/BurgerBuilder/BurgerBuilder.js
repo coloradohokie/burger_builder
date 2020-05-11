447f9b1c308221a4f0cb0fe6b22c1453a178b783
Michael Newman <coloradohokie@gmail.com>
2020-05-11 14:29:02 -0600

    pass ingredients via query params, navigate to contact data component, order submission, add orders page, implement navigation links

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props)
         axios.get('https://react-burger-builder-8a42d.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
@@ -91,28 +92,16 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue!')
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jenna Jameson',
-                address: {
-                    street: '69 Happy Ending Lane',
-                    phone: '1-800-976-4534'
-                },
-                email: 'jenna@jj.com'
-            },
-            deliveryMethod: 'fastest'
+       
+        const queryParams = []
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false})
-                console.log(error)
-            })
+        queryParams.push('price=' + this.state.totalPrice)
+        const queryString = queryParams.join('&')
+        this.props.history.push({
+            pathname: '/checkout',
+            search: queryString})
     }
 
 
