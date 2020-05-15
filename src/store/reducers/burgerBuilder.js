fb45b8d32b5ac220df6911c69d52fbd3a7603dd1
Michael Newman <coloradohokie@gmail.com>
2020-05-15 12:12:43 -0600

    refactor reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
     ingredients: null,
@@ -6,63 +7,63 @@ const initialState = {
     error: false
 }
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    bacon: 0.7,
+    meat: 1.3
+}
+
+const addIngredient = (state, action) => {
+    const updatedIngredient = {[action.ingredientType]: state.ingredients[action.ingredientType] + 1}
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType]
+    }
+    return updateObject(state, updatedState)
+}
+
+const removeIngredient = (state, action) => {
+    if (state.ingredients[action.ingredientType] <= 0) {
+        return state
+    }
+    
+    const updatedIng = {[action.ingredientType]: state.ingredients[action.ingredientType] - 1}
+    const updatedIngs = updateObject(state.ingredients, updatedIng)
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType]
+    }
+    return updateObject(state, updatedSt)
+}
+
+const setIngredients = (state, action) => {
+    const updatedSetState = {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: 4,
+        error: false
+    }
+    return updateObject (state, updatedSetState )    
+}
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, {error: true})    
+}
+
 const reducer = (state = initialState, action) => {
-    const INGREDIENT_PRICES = {
-        salad: 0.5,
-        cheese: 0.4,
-        bacon: 0.7,
-        meat: 1.3
-    }
-
-
     switch(action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientType]: state.ingredients[action.ingredientType] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientType]
-            }
-
-        case actionTypes.REMOVE_INGREDIENT:
-            if (state.ingredients[action.ingredientType] <= 0) {
-                return state
-            }
-            
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientType]: state.ingredients[action.ingredientType] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientType]
-            }
-
-        case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                totalPrice: 4,
-                error: false
-            }
-        
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            }
-
-        default:
-            return state
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action)
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action)
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state, action)
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action)
+        default: return state
     }
-
 }
 
 export default reducer
