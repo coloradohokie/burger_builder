ff582e3b5d485aa72c8401252d9103a8f7ac61e4
Michael Newman <coloradohokie@gmail.com>
2020-05-16 19:07:21 -0600

    add security, guard routes for unauthenticated users, only display orders specific to user.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 import classes from './App.module.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,19 +8,39 @@ import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import * as actions from './store/actions/index'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignUp()
+  }
+
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} /> 
+        <Redirect to="/" />            
+      </Switch>
+
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />  
+          <Redirect to="/" />            
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/" exact component={BurgerBuilder} />              
-            </Switch>
+            {routes}
         </Layout>
   
       </div>
@@ -27,4 +48,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignUp: () => dispatch(actions.authCheckState()) 
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
